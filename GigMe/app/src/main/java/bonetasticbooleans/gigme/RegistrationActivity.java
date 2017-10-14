23fe7c6fd7545686ca0584a1c18a561e4716e363
Alan Li <alanli9898@gatech.edu>
2017-10-14 02:37:14 -0400

    added RegistrationActivity.java

---
+++ GigMe/app/src/main/java/bonetasticbooleans/gigme/RegistrationActivity.java
@@ -0,0 +1,17 @@
+package bonetasticbooleans.gigme;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by alan on 10/14/17.
+ * Represents the activity launched when a user is entering a registration screen
+ */
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
